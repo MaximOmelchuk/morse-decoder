03da65661d646c58a5ae8d9bd167622a9df4d301
Maxim Omelchuk <94456038+MaximOmelchuk@users.noreply.github.com>
2022-01-21 15:37:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,24 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let fin=[]
+    
+    expr=expr.split('**********')
+             .map(x=>{
+                let tempArr=[];
+                while (x.length>0) {
+                    tempArr.push(x.slice(0,10));
+                    x=x.slice(10);
+                }
+                return tempArr;
+             })
+             .map( x=> x.map(y=>y.slice(y.indexOf('1'))) );
+
+
+    return expr.map(x=>x.map(y=>y.replace(/10/g, '.').replace(/11/g, '-')))
+               .map(x=>x.map(y=>MORSE_TABLE[y]).join(''))
+               .join(' ');
+
 }
 
 module.exports = {
